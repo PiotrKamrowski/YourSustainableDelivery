d79b8984b057fd50bccb963df0bfcbc217cf372f
Piotr Kamrowski <piotr.kamrowski@wp.pl>
2017-12-19 09:19:31 +0100

    add project configuration

---
+++ src/main/java/com/yoursustainabledelivery/config/ApplicationInitializer.java
@@ -0,0 +1,25 @@
+package com.yoursustainabledelivery.config;
+
+import org.springframework.web.WebApplicationInitializer;
+import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.servlet.DispatcherServlet;
+
+import javax.servlet.ServletContext;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRegistration;
+
+public class ApplicationInitializer implements WebApplicationInitializer {
+
+
+    @Override
+    public void onStartup(ServletContext container) throws ServletException {
+
+        AnnotationConfigWebApplicationContext ctx =
+                new AnnotationConfigWebApplicationContext(); ctx.register(ApplicationConfiguration.class); ctx.setServletContext(container); ServletRegistration.Dynamic servlet =
+                container.addServlet("dispatcher",
+                        new DispatcherServlet(ctx)); servlet.setLoadOnStartup(1); servlet.addMapping("/");
+    }
+}
+
+
+
